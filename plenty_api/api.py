voodoo3300/python_plentymarkets_api

commit 90896e5c04804f3771d7d60b59a88968b9dd7afc

Fix: Add missing valid values for get_storagelocations
The route provides an additional refine and a additional value, which
were missing from the valid values table. Add the missing values.

Fixes: f714388 Add more GET routes

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/constants.py
@@ -93,7 +93,7 @@ VALID_REFINE_KEYS = {
         'variationId',
     ],
     'warehouses': [
-        'variationId',
+        'variationId', 'storageLocationId'
     ],
     'contact': [
         'fullText', 'contactEmail', 'email', 'postalCode', 'plentyId',
@@ -139,6 +139,9 @@ VALID_ADDITIONAL_VALUES = {
     'contact': [
         'addresses', 'accounts', 'options', 'orderSummary',
         'primaryBillingAddress', 'contactOrders'
+    ],
+    'warehouses': [
+        'storageLocation'
     ]
 }
 

commit efaf51b259c148d9a2d723586069a4249721c5ee

Fix: Replace deprecated additional storageLocation parameter
The usage of `?with[]=storageLocation` is deprecated and replaced by
`?with[]=warehouseLocation`. Adjust the constant additional values
accordingly, as well as the documentation.

Fixes: 90896e5 Fix: Add missing valid values for get_storagelocations

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -757,7 +757,7 @@ class PlentyApi():
             additional  [list]  -   Add additional elements to the response
                                     data.
                                     Example:
-                                    ['storageLocation']
+                                    ['warehouseLocation']
 
         Return:
                         [JSON(Dict) / DataFrame] <= self.data_format

plenty_api/constants.py
@@ -141,7 +141,7 @@ VALID_ADDITIONAL_VALUES = {
         'primaryBillingAddress', 'contactOrders'
     ],
     'warehouses': [
-        'storageLocation'
+        'warehouseLocation'
     ]
 }
 