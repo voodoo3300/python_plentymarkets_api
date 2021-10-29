voodoo3300/python_plentymarkets_api

commit 3bdbf9e76e20f823f85de4b8333e170dabe42905

Fix: utils: Convert ID to integer before comparision
Enable the user to supply variations IDs within the template as string
by converting the IDs to integers before comparing them to the API
response. Properly handle incorrect variation IDs with an error message.

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/utils.py
@@ -538,10 +538,18 @@ def build_transactions(order: dict, variations: dict,
     outgoing = []
     incoming = []
     for item in order['orderItems']:
-        template_variation = [
-            x for x in variations
-            if x['variation_id'] == item['itemVariationId']
-        ][0]
+        try:
+            template_variation = [
+                x for x in variations
+                if int(x['variation_id']) == item['itemVariationId']
+            ][0]
+        except ValueError:
+            invalid_variations = [x for x in variations
+                                  if (isinstance(x, str) and not x.isdigit())
+                                  or not isinstance(x, str)]
+            logging.error("Invalid variaition ID value within template "
+                          f"({invalid_variations})")
+            break
         if 'locations' not in template_variation.keys():
             continue
         kwargs = {}

commit 719c6d51274bba21e609ce80fb4200f9ae146be8

Fix: Correct typo in build_transactions
s/variaition/variation/

Fixes: 3bdbf9e7 Convert ID to integer before comparision

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/utils.py
@@ -547,7 +547,7 @@ def build_transactions(order: dict, variations: list,
             invalid_variations = [x for x in variations
                                   if (isinstance(x, str) and not x.isdigit())
                                   or not isinstance(x, str)]
-            logging.error("Invalid variaition ID value within template "
+            logging.error("Invalid variation ID value within template "
                           f"({invalid_variations})")
             break
         if 'locations' not in template_variation.keys():