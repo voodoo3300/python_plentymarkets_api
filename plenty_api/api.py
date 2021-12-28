voodoo3300/python_plentymarkets_api

commit 890abf803b237bf754aefc5b566043c1a4fe871e

Feature: Get property selection mapping
Pull the property selection data from the Plentymarkets Rest API and
create a dictionary with a property ID -> selection ID -> language ->
value structure to enable the user to get the appropriate values for
their selection IDs when pulling their variation property data.

Signed-off-by: Jan <jan.sallermann@panasiam.de>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -27,6 +27,8 @@ import simplejson
 import gnupg
 import logging
 import tqdm
+import pandas
+from collections import defaultdict
 from datetime import datetime, timezone
 
 import plenty_api.keyring
@@ -65,6 +67,8 @@ class PlentyApi():
         **plenty_api_get_variation_warehouses**
 
         **plenty_api_get_contacts**
+
+        **plenty_api_get_property_selections**
 
         POST REQUESTS
         **plenty_api_set_image_availability**
@@ -836,6 +840,47 @@ class PlentyApi():
             domain='contact',
             refine=refine,
             additional=additional)
+
+    def plenty_api_get_property_selections(self, refine: dict = None):
+        """
+        Get a mapping of selection IDs to the actual value in all available
+        languages.
+
+        Parameter:
+            refine      [dict]  -   Apply filters to the request
+                                    Example: {'propertyId': 123}
+
+        Return:
+                        [dict]  -   Mapping of all selection property values
+        """
+        domain='property',
+        path='/selections',
+
+        query = utils.sanity_check_parameter(
+            domain=domain, query=query, refine=refine,
+            additional=None, lang='')
+
+        data = self.__repeat_get_request_for_all_records(
+            domain=domain, path=path, query=query)
+
+        selection_map = defaultdict(lambda: defaultdict(dict))
+        for selection in data:
+            key = selection['propertyId']
+            value_id = selection['id']
+            for value in selection['relation']['relationValues']:
+                selection_map[key][value_id][value['lang']] = value['value']
+
+        df_data = []
+        if self.data_format == 'dataframe':
+            for property_id, selections in data.items():
+                for selection_id, languages in selections.items():
+                    for language, name in languages.items():
+                        df_data.append([property_id, selection_id, language,
+                                        name])
+            columns = ['property_id', 'selection_id', 'language', 'name']
+            return pandas.DataFrame(df_data, columns=columns)
+
+        return selection_map
 
 # POST REQUESTS
 

plenty_api/constants.py
@@ -34,7 +34,8 @@ VALID_DOMAINS = [
     'stockmanagement',
     'variation',
     'vat',
-    'warehouses'
+    'warehouses',
+    'property'
 ]
 VALID_ROUTES = [
     '/rest/items/attributes',
@@ -48,7 +49,8 @@ VALID_ROUTES = [
     '/rest/stockmanagement/stock',
     '/rest/items/variations',
     '/rest/vat',
-    '/rest/stockmanagement/warehouses'
+    '/rest/stockmanagement/warehouses',
+    '/rest/properties'
 ]
 DOMAIN_ROUTE_MAP = dict(zip(VALID_DOMAINS, VALID_ROUTES))
 
@@ -102,6 +104,9 @@ VALID_REFINE_KEYS = {
         'contactAddress', 'countryId', 'userId', 'referrerId', 'name',
         'nameOrId', 'town', 'privatePhone', 'billingAddressId',
         'deliveryAddressId', 'tagIds'
+    ],
+    'property': [
+        'propertyId'
     ]
 }
 

commit 0247135e8b3deeb30421366f58a865889f2ea639

Fix: Convert defaultdict to ordinary dict
Return a dictonary instead of a defaultdict to return the expected data
type to the user.

Fixes: 890abf80 Get property selection mapping

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -1078,7 +1078,7 @@ class PlentyApi():
             columns = ['property_id', 'selection_id', 'language', 'name']
             return pandas.DataFrame(df_data, columns=columns)
 
-        return selection_map
+        return dict(selection_map)
 
     def plenty_api_get_property_selection_names(self, selection_id: int):
         """