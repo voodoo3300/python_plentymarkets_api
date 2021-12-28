voodoo3300/python_plentymarkets_api

commit bf911772dfba7560215f55827440516b4e24d233

Feature: Create a new for property selection name
Create a new name for a property selection by passing a single
JSON to the REST API.

Signed-off-by: Jan <jan.sallermann@panasiam.de>
Reviewed-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -91,6 +91,8 @@ class PlentyApi():
 
         **plenty_api_create_booking**
 
+        **plenty_api_create_property_selection_name**
+
         PUT REQUESTS
 
         **plenty_api_update_redistribution**
@@ -1233,6 +1235,35 @@ class PlentyApi():
                                              domain="order",
                                              path=path,
                                              data=data)
+        return response
+
+
+    def plenty_api_create_property_selection_name(self, property_id: int,
+                                                  lang: str, name: str) -> dict:
+        """
+        Create a name in a specific language for a selection property.
+
+        Parameter:
+            property_id     [int]   -   Plentymarkets property Id which a new
+                                        selection namm will be created for
+            lang            [str]   -   Initial language for selection creation
+            name            [str]   -   Value of the selection name
+
+        Return:
+                            [dict]  -   Return the response object, in case the
+                                        request fails return an error message
+        """
+        if not property_id or not lang or not name:
+            return [{'error': 'missing_parameter'}]
+
+        if utils.get_language(lang=lang) == 'INVALID_LANGUAGE':
+            return {'error': 'invalid_language'}
+
+        json = {'propertyId': property_id, 'lang': lang, 'name': name,}
+
+        path = "/selections/names"
+        response = self.__plenty_api_request(method="post", path=path,
+                                             domain="v2property", data=json)
         return response
 
 # PUT REQUESTS

plenty_api/constants.py
@@ -35,7 +35,8 @@ VALID_DOMAINS = [
     'variation',
     'vat',
     'warehouses',
-    'property'
+    'property',
+    'v2property'
 ]
 VALID_ROUTES = [
     '/rest/items/attributes',
@@ -50,7 +51,8 @@ VALID_ROUTES = [
     '/rest/items/variations',
     '/rest/vat',
     '/rest/stockmanagement/warehouses',
-    '/rest/properties'
+    '/rest/properties',
+    '/rest/v2/properties'
 ]
 DOMAIN_ROUTE_MAP = dict(zip(VALID_DOMAINS, VALID_ROUTES))
 

commit 6955572cf034e4838c72d0833a21b69fdb395ea2

Fix: Add required field for POST request
The POST request demands a selection ID to create the new name for.
Add the required parameter and add documentation.
Additionally, Fix a typo and an incorrect return data type.

Fixes: bf911772 Create a new for property selection name

Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

plenty_api/api.py
@@ -1545,14 +1545,15 @@ class PlentyApi():
         return response
 
 
-    def plenty_api_create_property_selection_name(self, property_id: int,
-                                                  lang: str, name: str) -> dict:
+    def plenty_api_create_property_selection_name(
+        self, property_id: int, selection_id: int, lang: str, name: str
+    ) -> dict:
         """
         Create a name in a specific language for a selection property.
 
         Parameter:
             property_id     [int]   -   Plentymarkets property Id which a new
-                                        selection namm will be created for
+                                        selection name will be created for
             lang            [str]   -   Initial language for selection creation
             name            [str]   -   Value of the selection name
 
@@ -1561,12 +1562,15 @@ class PlentyApi():
                                         request fails return an error message
         """
         if not property_id or not lang or not name:
-            return [{'error': 'missing_parameter'}]
+            return {'error': 'missing_parameter'}
 
         if utils.get_language(lang=lang) == 'INVALID_LANGUAGE':
             return {'error': 'invalid_language'}
 
-        json = {'propertyId': property_id, 'lang': lang, 'name': name}
+        json = {
+            'propertyId': property_id, 'selectionId': selection_id,
+            'lang': lang, 'name': name
+        }
 
         path = "/selections/names"
         response = self.__plenty_api_request(method="post", path=path,